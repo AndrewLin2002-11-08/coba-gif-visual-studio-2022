c0f6e0c7e4ab2eedd2f58b38515fa5e582ea9e44
Andrew Lin <Andrew Lin@LAPTOP-B21CI6OA>
2021-12-03 09:03:05 +0700

    Add project files.

---
+++ coba gif 2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coba_gif_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
